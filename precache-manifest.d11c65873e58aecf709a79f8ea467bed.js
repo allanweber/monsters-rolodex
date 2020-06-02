e941e3df29ddeebfc3fb649bbd0b6821d00d37c3
Allan Weber <a.cassianoweber@gmail.com>
2020-06-02 20:19:44 +0200

    Updates

---
+++ precache-manifest.d11c65873e58aecf709a79f8ea467bed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "150e4fac76b05526d65bdd54f35f9335",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "78721f1d74b8b1f38f68",
+    "url": "/monsters-rolodex/static/css/main.ec8248a8.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78721f1d74b8b1f38f68",
+    "url": "/monsters-rolodex/static/js/main.9d85e9f9.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
